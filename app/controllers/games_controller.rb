37b2e9b5719fca295cea8cb8bb388f7f364135c5
Oliver <deftool@outlook.com>
2016-05-29 17:01:11 -0700

    Update User balnce

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -44,20 +44,36 @@ class GamesController < ApplicationController
     @game = Game.find_by(id: params[:id])
 
     @bets = Bet.where(game_id: @game.id)
+
+    # @subtotal_fund = Game.find_by(id: @game.id).bet_amount
     # @banker = Bet.find_by(id: params[:id]).banker_id
+    @pot = @game.bet_amount * 90
+
+
+
 
     @winner = @game.winning_score
     @loser = @game.losing_score
-    @winning_combo = (@winner % 10).to_s + "-" + (@loser % 10).to_s
-    @winning_combo_id = Combo.find_by(pick: @winning_combo).id
-    @winning_user = User.find((Bet.find_by(combo_id: @winning_combo_id).better_id))
+    if @winner && @loser 
+        @winning_combo = ((@winner % 10).to_s + "-" + (@loser % 10).to_s)
+        @winning_combo_id = Combo.find_by(pick: @winning_combo).id
+        @winning_user = User.find((Bet.find_by(combo_id: @winning_combo_id).better_id)) 
+        @total_balance = @winning_user.balance + @pot
+        @winning_user.update_attributes({'balance': @total_balance})
+    end
+    
+
+    
+
+
     # amount = "formula for amount won"
     # newbalance = User.find(params[:id]).balance + amount
     # User.update(balance: newbalance)
 
-
-
   end
-
-
 end
+
+
+
+
+
--- db/migrate/20160522223812_add_winning_combo_to_game.rb
+++
@@ -1,5 +0,0 @@
-class AddWinningComboToGame < ActiveRecord::Migration
-  def change
-    add_column :games, :winning_
-  end
-end
